b5d48ea09648eba274702bbc3ff0e7903dfbb901
Chopper <20141112@lamolina.edu.pe>
2023-04-19 14:09:35 -0500

    the etl process about sales and debtors data was improved, also a simple sql query was uploaded and some excel sample files

--- etl_process.py
+++ etl_process.py
@@ -10,7 +10,11 @@ engine = create_engine('postgresql://postgres:rufo2324@localhost:5432/postgres')
 # Reading the xlsx file 
 df = pd.read_excel("C:/Users/chopper/Downloads/ventas.xlsx", skiprows=1)
 
-# uploading the data
+# Renaming the column to have a common key name between both tables (ventas/cobranzas)
+df.rename(columns = {'Serie-Núm.':'document_number'}, inplace = True)
+
+
+# Uploading the data
 with engine.begin() as connection:
         df.to_sql('ventas', con=connection, index_label='id', if_exists='replace')
         print('The sales data was uploaded correctly')
@@ -30,6 +34,8 @@ cobranza_first_100=cobranza_first_100[
         "Deuda Total",
         "Abonos Total"]]
 
+cobranza_first_100["document_number"] = cobranza_first_100["Documento"].str[15:]
+cobranza_first_100["Documento"] = cobranza_first_100["Documento"].str[:13]
 
 # This second xlsx contain the rows from one hundred onwards
 cobranza_100_to_200 = pd.read_excel("C:/Users/chopper/Downloads/cobros_2.xlsx", skiprows=1)
@@ -40,10 +46,15 @@ cobranza_100_to_200=cobranza_100_to_200[
         "Deuda Total",
         "Abonos Total"]]
 
+cobranza_100_to_200["document_number"] = cobranza_100_to_200["Documento"].str[15:]
+cobranza_100_to_200["Documento"] = cobranza_100_to_200["Documento"].str[:13]
+
 # "UNION ALL" TO BOTH DATAFRAMES
 cobranzas = pd.concat([cobranza_first_100,cobranza_100_to_200])
 
-# uploading the data
+# Uploading the data
 with engine.begin() as connection:
         cobranzas.to_sql('cobranzas', con=connection, index_label='id', if_exists='replace')
         print('The debtors data was uploaded correctly')
+
+
