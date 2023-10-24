b1f8944b952f1b3bbd561b0828aa979de655639a
chopper <luisbarrios2197@gmail.com>
2023-10-24 12:13:28 -0500

    awa

--- cobranzas/app.py
+++ cobranzas/app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-   return "Hello World!"
+  name = "Juan Pérez"
+  return render_template('C:\Users\chopper\Documents\Lydemar\cobranzas\index.html', name=name)
+
 if __name__ == "__main__":
-   app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 5000)))
+  app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 5000)))
