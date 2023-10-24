f1d4e1df889ed5175cc080faa8cef4ff526a6fe4
chopper <luisbarrios2197@gmail.com>
2023-10-24 16:14:50 -0500

    cambios realizados

--- cobranzas/app.py
+++ cobranzas/app.py
@@ -1,12 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__)
+app = Flask(__name__ , template_folder=r'cobranzas\templates' ,static_folder='./cobranzas/static')
 
+# Rutas
 @app.route('/')
 def index():
-  name = "Juan Pérez"
-  return render_template('C:\Users\chopper\Documents\Lydemar\cobranzas\index.html', name=name)
+  return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/submit', methods=['POST'])
+def submit():
+  fecha = request.form['fecha']
+  nombre_cliente = request.form['nombre_cliente']
+  factura_o_nota_de_pedido = request.form['factura_o_nota_de_pedido']
+  n_documento = request.form['n_documento']
+  producto = request.form['producto']
+  cantidad_en_cajas_o_sacos = request.form['cantidad_en_cajas_o_sacos']
+  precio_x_caja_o_saco = request.form['precio_x_caja_o_saco']
+  totales_en_soles = request.form['totales_en_soles']
+  pago_a_cuenta_efectivo_yape_transferencia = request.form['pago_a_cuenta_efectivo_yape_transferencia']
+  debe = request.form['debe']
+  return render_template('submitted.html',
+                          fecha=fecha, 
+                          nombre_cliente=nombre_cliente, 
+                          factura_o_nota_de_pedido=factura_o_nota_de_pedido, 
+                          n_documento=n_documento, 
+                          producto=producto, 
+                          cantidad_en_cajas_o_sacos=cantidad_en_cajas_o_sacos, 
+                          precio_x_caja_o_saco=precio_x_caja_o_saco, 
+                          totales_en_soles=totales_en_soles, 
+                          pago_a_cuenta_efectivo_yape_transferencia=pago_a_cuenta_efectivo_yape_transferencia, 
+                          debe=debe)
+
+
+# Iniciar el servidor
+if __name__ == '__main__':
   app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 5000)))
