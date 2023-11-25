2943d6ceb350dd8ec262441653332f54d01f199b
chopper <luisbarrios2197@gmail.com>
2023-11-24 21:14:03 -0500

    new inputs

--- cobranzas/app.py
+++ cobranzas/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, url_for , jsonify, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_utils import create_view
 from sqlalchemy import text
 import os
 import itertools
